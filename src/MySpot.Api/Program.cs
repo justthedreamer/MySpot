044ec255cd271d853142f66e92fee26498b92066
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-19 17:27:47 +0100

    feat : Make clock abstract and register in IOC

--- src/MySpot.Api/Program.cs
+++ src/MySpot.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ReservationDto>();
+builder.Services.AddSingleton<IClock, Clock>();
 
 var app = builder.Build();
 app.MapControllers();
--- src/MySpot.Api/Services/Clock.cs
+++ src/MySpot.Api/Services/Clock.cs
@@ -1,6 +1,6 @@
 namespace MySpot.Api.Services;
 
-public class Clock
+public class Clock : IClock
 {
     public DateTime Current() => DateTime.UtcNow;
 }
