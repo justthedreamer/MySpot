6172b7f2e5a59d1274bf562375e761d776c0dda0
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-19 17:13:03 +0100

    update

--- src/MySpot.Api/Program.cs
+++ src/MySpot.Api/Program.cs
@@ -3,7 +3,6 @@ using MySpot.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ReservationsService>();
 
 var app = builder.Build();
 app.MapControllers();
