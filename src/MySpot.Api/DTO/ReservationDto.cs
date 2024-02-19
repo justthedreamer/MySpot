4b6462144096530ae760ceefcd100211c7a93aea
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-19 17:12:39 +0100

    feat : ReservationDto created

---
+++ src/MySpot.Api/DTO/ReservationDto.cs
@@ -0,0 +1,9 @@
+namespace MySpot.Api.DTO;
+
+public class ReservationDto
+{
+    public Guid Id { get; set; }
+    public Guid ParkingSpotId { get; set; }
+    public string LicensePlate { get; set; }
+    public DateTime Date { get; set; }
+}
