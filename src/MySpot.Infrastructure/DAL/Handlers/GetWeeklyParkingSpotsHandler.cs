9c3943da43772bf6912f917042a98e762992a3a6
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-26 21:39:39 +0100

    feat : Create Get Weekly Parking Spots Query Handler

---
+++ src/MySpot.Infrastructure/DAL/Handlers/GetWeeklyParkingSpotsHandler.cs
@@ -0,0 +1,25 @@
+using Humanizer;
+using Microsoft.AspNetCore.Routing.Matching;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MySpot.Application.Abstractions;
+using MySpot.Application.DTO;
+using MySpot.Application.Queries;
+using MySpot.Core.ValueObjects;
+
+namespace MySpot.Infrastructure.DAL.Handlers;
+
+internal sealed class GetWeeklyParkingSpotsHandler(MySpotDbContext dbContext)
+    : IQueryHandler<GetWeeklyParkingSpots, IEnumerable<WeeklyParkingSpotDto>>
+{
+    public async Task<IEnumerable<WeeklyParkingSpotDto>> HandleAsync(GetWeeklyParkingSpots query)
+    {
+        var week = query.Date.HasValue ? new Week(query.Date.Value) : null;
+        return await dbContext.WeeklyParkingSpots
+            .Where(wps => wps.Week == week || week == null)
+            .Include(ps => ps.Reservations)
+            .AsNoTracking()
+            .Select(ps => ps.GetDto())
+            .ToListAsync();
+    }
+}
