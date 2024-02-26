684edb9e1fb1ed10bf32e2da29fc7ee3cee33247
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-26 21:41:32 +0100

    feat : create interace method for GetWeeklyParkingSpotByReservationId

--- src/MySpot.Core/Repositories/IWeeklyParkingSpotRepository.cs
+++ src/MySpot.Core/Repositories/IWeeklyParkingSpotRepository.cs
@@ -8,6 +8,7 @@ public interface IWeeklyParkingSpotRepository
     Task<WeeklyParkingSpot?> GetAsync(ParkingSpotId id);
     Task<IEnumerable<WeeklyParkingSpot>> GetAllAsync();
     Task<IEnumerable<WeeklyParkingSpot>> GetByWeekAsync(Week week) => throw new NotImplementedException();
+    Task<WeeklyParkingSpot> GetWeeklyByReservationId(ReservationId id) => throw new NotImplementedException();
     Task AddAsync(WeeklyParkingSpot weeklyParkingSpot);
     Task UpdateAsync(WeeklyParkingSpot weeklyParkingSpot);
     Task DeleteAsync(WeeklyParkingSpot weeklyParkingSpot);
