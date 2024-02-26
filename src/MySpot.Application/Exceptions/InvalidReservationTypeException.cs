eca377d8013f360288fa7c7e0d8ce52977517c99
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-26 21:38:05 +0100

    feat : Create custom exception for reservation invalid type.

---
+++ src/MySpot.Application/Exceptions/InvalidReservationTypeException.cs
@@ -0,0 +1,9 @@
+using MySpot.Core.Entities;
+using MySpot.Core.Exceptions;
+
+namespace MySpot.Application.Exceptions;
+
+public sealed class InvalidReservationTypeException(Reservation reservation) : CustomException($"Invalid reservation type: {nameof(reservation)}")
+{
+    
+}
