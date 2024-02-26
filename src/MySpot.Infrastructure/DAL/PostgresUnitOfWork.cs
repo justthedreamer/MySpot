d878789715f1001b45394a867a2a6718359c6d78
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-26 23:59:41 +0100

    feat : Create db transaction implementation

---
+++ src/MySpot.Infrastructure/DAL/PostgresUnitOfWork.cs
@@ -0,0 +1,21 @@
+namespace MySpot.Infrastructure.DAL;
+
+internal sealed class PostgresUnitOfWork(MySpotDbContext dbContext) : IUnitOfWork
+{
+    public async Task ExecuteAsync(Func<Task> action)
+    {
+        await using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+        try
+        {
+            await action();
+            await dbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
+    }
+}
