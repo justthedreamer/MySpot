2c44cbb5854004776253b576ec94249dd2932505
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-27 02:55:47 +0100

    cleanup

--- src/MySpot.Api/Controllers/ParkingSpotsController.cs
+++ src/MySpot.Api/Controllers/ParkingSpotsController.cs
@@ -3,8 +3,6 @@ using MySpot.Application.Abstractions;
 using MySpot.Application.Commands;
 using MySpot.Application.DTO;
 using MySpot.Application.Queries;
-using MySpot.Application.Services;
-using MySpot.Core.Entities;
 
 namespace MySpot.Api.Controllers;
 
@@ -19,13 +17,14 @@ public class ParkingSpotsController(
     IQueryHandler<GetWeeklyParkingSpot, WeeklyParkingSpotDto> getWeeklyParkingSpotHandler)
     : ControllerBase
 {
-
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<WeeklyParkingSpotDto>>> Get([FromQuery] GetWeeklyParkingSpots command) => 
-        Ok(await getWeeklyParkingSpotsHandler.HandleAsync(command));
+    public async Task<ActionResult<IEnumerable<WeeklyParkingSpotDto>>> Get([FromQuery] GetWeeklyParkingSpots command)
+    {
+        return Ok(await getWeeklyParkingSpotsHandler.HandleAsync(command));
+    }
 
     [HttpPost("{parkingSpotId:guid}/reservations/vehicle")]
-    public async Task<ActionResult> Post(Guid parkingSpotId,ReserveParkingSpotForVehicle command)
+    public async Task<ActionResult> Post(Guid parkingSpotId, ReserveParkingSpotForVehicle command)
     {
         await reserveParkingSpotForVehicleHandler.HandleAsync(command with
         {
@@ -42,7 +41,7 @@ public class ParkingSpotsController(
         await reserveParkingSpotForCleaningHandler.HandleAsync(command);
         return NoContent();
     }
-    
+
     [HttpPut("reservations/{reservationId:guid}")]
     public async Task<ActionResult> Put(ChangeReservationLicensePlate command)
     {
