b9489e167d94ab8c503b8658c7260cde98221b2f
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-27 02:57:30 +0100

    feat : Add UserDto convertion method

--- src/MySpot.Application/DTO/MapDto.cs
+++ src/MySpot.Application/DTO/MapDto.cs
@@ -4,19 +4,35 @@ namespace MySpot.Application.DTO;
 
 public static class MapDto
 {
-    public static ReservationDto GetDto(this Reservation reservation) => new ReservationDto(
-            Id: reservation.Id,
-            ParkingSpotId: reservation.ParkingSpotId,
-            EmployeeName: reservation is VehicleReservation vs ? vs.EmployeeName : string.Empty,
-            Type: nameof(reservation),
-            Date: reservation.Date.Value.Date);
+    public static ReservationDto GetDto(this Reservation reservation)
+    {
+        return new ReservationDto(
+            reservation.Id,
+            reservation.ParkingSpotId,
+            reservation is VehicleReservation vs ? vs.EmployeeName : string.Empty,
+            nameof(reservation),
+            reservation.Date.Value.Date);
+    }
 
-    public static WeeklyParkingSpotDto GetDto(this WeeklyParkingSpot weeklyParkingSpot) => new WeeklyParkingSpotDto(
-        Id: weeklyParkingSpot.Id,
-        Name: weeklyParkingSpot.Name,
-        From: weeklyParkingSpot.Week.From.Value.Date,
-        To: weeklyParkingSpot.Week.From.Value.Date,
-        Capacity: weeklyParkingSpot.Capacity,
-        Reservations: weeklyParkingSpot.Reservations.Select(r => r.GetDto())
-    );
+    public static WeeklyParkingSpotDto GetDto(this WeeklyParkingSpot weeklyParkingSpot)
+    {
+        return new WeeklyParkingSpotDto(
+            weeklyParkingSpot.Id,
+            weeklyParkingSpot.Name,
+            weeklyParkingSpot.Week.From.Value.Date,
+            weeklyParkingSpot.Week.From.Value.Date,
+            weeklyParkingSpot.Capacity,
+            weeklyParkingSpot.Reservations.Select(r => r.GetDto())
+        );
+    }
+
+    public static UserDto GetDto(this User user)
+    {
+        return new UserDto(
+            Id: user.Id,
+            Username: user.Username,
+            Fullname: user.FullName
+        );
+
+    }
 }
