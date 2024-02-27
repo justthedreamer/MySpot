05b90e48726c5dce3812dcd75ad9bfe4fb79ed75
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-27 02:56:46 +0100

    cleanup

--- src/MySpot.Application/Commands/Handlers/DeleteReservationHandler.cs
+++ src/MySpot.Application/Commands/Handlers/DeleteReservationHandler.cs
@@ -4,20 +4,18 @@ using MySpot.Core.Repositories;
 
 namespace MySpot.Application.Commands.Handlers;
 
-public sealed class DeleteReservationHandler(IWeeklyParkingSpotRepository repository) : ICommandHandler<DeleteReservation>
+public sealed class DeleteReservationHandler(IWeeklyParkingSpotRepository repository)
+    : ICommandHandler<DeleteReservation>
 {
     public async Task HandleAsync(DeleteReservation command)
     {
         var weeklyParkingSpot = await repository.GetWeeklyByReservationId(command.ReservationId);
 
-        if (weeklyParkingSpot is null)
-        {
-            throw new ReservationNotFoundException(command.ReservationId);
-        }
+        if (weeklyParkingSpot is null) throw new ReservationNotFoundException(command.ReservationId);
 
         var reservation = weeklyParkingSpot.Reservations
             .Single(x => x.Id.Value == command.ReservationId);
-            weeklyParkingSpot
+        weeklyParkingSpot
             .RemoveReservation(reservation);
 
         await repository.UpdateAsync(weeklyParkingSpot);
--- src/MySpot.Application/Commands/ReserveParkingSpotForVehicle.cs
+++ src/MySpot.Application/Commands/ReserveParkingSpotForVehicle.cs
@@ -2,4 +2,10 @@ using MySpot.Application.Abstractions;
 
 namespace MySpot.Application.Commands;
 
-public record ReserveParkingSpotForVehicle(Guid ParkingSpotId, Guid ReservationId, string EmployeeName, string LicensePlate, DateTimeOffset Date,int Capacity) : ICommand;
+public record ReserveParkingSpotForVehicle(
+    Guid ParkingSpotId,
+    Guid ReservationId,
+    string EmployeeName,
+    string LicensePlate,
+    DateTimeOffset Date,
+    int Capacity) : ICommand;
