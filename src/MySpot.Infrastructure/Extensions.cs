f44e3777987d8fa0c1807642d8836cd7dfd19a0b
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-27 02:55:24 +0100

    clean : Remove unnecessary usings

--- src/MySpot.Infrastructure/Extensions.cs
+++ src/MySpot.Infrastructure/Extensions.cs
@@ -1,20 +1,16 @@
 using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MySpot.Application.Abstractions;
-using MySpot.Application.Services;
 using MySpot.Core.Abstractions;
-using MySpot.Core.Entities;
-using MySpot.Core.ValueObjects;
 using MySpot.Infrastructure.DAL;
 using MySpot.Infrastructure.Exceptions;
 using MySpot.Infrastructure.Logging;
+using MySpot.Infrastructure.Security;
 using MySpot.Infrastructure.Time;
 
-[assembly:InternalsVisibleTo("MySpot.test.unit")]
+[assembly: InternalsVisibleTo("MySpot.test.unit")]
 namespace MySpot.Infrastructure;
 
 public static class Extensions
