4aebf034e53569abf1833ebfb0d6cb471791e59c
justthedreamer <piotr.zapala.dev@gmail.com>
2024-02-27 02:45:25 +0100

    update : Cleanup

--- src/MySpot.Core/ValueObjects/Date.cs
+++ src/MySpot.Core/ValueObjects/Date.cs
@@ -2,34 +2,52 @@ namespace MySpot.Core.ValueObjects;
 
 public sealed record Date
 {
-    public DateTimeOffset Value { get; }
-
     public Date(DateTimeOffset value)
     {
         Value = value.Date;
     }
-    
-    public Date AddDays(int days) => new(Value.AddDays(days));
 
-    public static implicit operator DateTimeOffset(Date date)
-        => date.Value;
-    
-    public static implicit operator Date(DateTimeOffset value)
-        => new(value);
-
-    public static bool operator <(Date date1, Date date2)
-        => date1.Value < date2.Value;
-
-    public static bool operator >(Date date1, Date date2)
-        => date1.Value > date2.Value;
-
-    public static bool operator <=(Date date1, Date date2)
-        => date1.Value <= date2.Value;
-    
-    public static bool operator >=(Date date1, Date date2)
-        => date1.Value >= date2.Value;
+    public DateTimeOffset Value { get; }
 
     public static Date Now => new(DateTimeOffset.Now);
 
-    public override string ToString() => Value.ToString("d");
+    public Date AddDays(int days)
+    {
+        return new Date(Value.AddDays(days));
+    }
+
+    public static implicit operator DateTimeOffset(Date date)
+    {
+        return date.Value;
+    }
+
+    public static implicit operator Date(DateTimeOffset value)
+    {
+        return new Date(value);
+    }
+
+    public static bool operator <(Date date1, Date date2)
+    {
+        return date1.Value < date2.Value;
+    }
+
+    public static bool operator >(Date date1, Date date2)
+    {
+        return date1.Value > date2.Value;
+    }
+
+    public static bool operator <=(Date date1, Date date2)
+    {
+        return date1.Value <= date2.Value;
+    }
+
+    public static bool operator >=(Date date1, Date date2)
+    {
+        return date1.Value >= date2.Value;
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString("d");
+    }
 }
--- src/MySpot.Core/ValueObjects/LicencePlate.cs
+++ src/MySpot.Core/ValueObjects/LicencePlate.cs
@@ -4,25 +4,23 @@ namespace MySpot.Core.ValueObjects;
 
 public sealed record LicencePlate
 {
-    public string Value { get; }
-
     public LicencePlate(string value)
     {
-        if (string.IsNullOrWhiteSpace(value))
-        {
-            throw new InvalidLicencePlateException(value);
-        }
-        if (value.Length is < 5 or > 8)
-        {
-            throw new InvalidLicencePlateException(value);
-        }
-        
+        if (string.IsNullOrWhiteSpace(value)) throw new InvalidLicencePlateException(value);
+        if (value.Length is < 5 or > 8) throw new InvalidLicencePlateException(value);
+
         Value = value;
     }
 
+    public string Value { get; }
+
     public static implicit operator string(LicencePlate licencePlate)
-        => licencePlate.Value;
+    {
+        return licencePlate.Value;
+    }
 
     public static implicit operator LicencePlate(string value)
-        => new(value);
+    {
+        return new LicencePlate(value);
+    }
 }
